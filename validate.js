3cebc6c6ec718a70ec96dea293540095a983eecc
Suryaviyyapu <dhanush.surya01@gmail.com>
2020-05-05 20:07:46 +0530

    Initial Commit

---
+++ validate.js
@@ -0,0 +1,16 @@
+
+		function validateForm() {
+			var un = document.loginform.usr.value;
+			var pw = document.loginform.pword.value;
+			var username = "admin@mystartup.com";
+			var password = "admin";
+			if ((un === username) && (pw === password)) {
+                alert("FLAG_CMRCET_3{ W3AK_P4SSW0RDS_AR3_D4NG3R }");
+				return true;
+			}
+			else {
+				alert ("Login was unsuccessful, please check your username and password");
+				return false;
+			}
+		}
+
