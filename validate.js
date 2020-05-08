09a4c3f5d1350ea311a4bf7a3813bb2bb72aac11
csclub-cmrcet <63036394+csclub-cmrcet@users.noreply.github.com>
2020-05-08 20:16:10 +0530

    Update validate.js

--- validate.js
+++ validate.js
@@ -2,7 +2,7 @@
 		function validateForm() {
 			var un = document.loginform.usr.value;
 			var pw = document.loginform.pword.value;
-			var username = "admin@mystartup.com";
+			var username = "admin";
 			var password = "admin";
 			if ((un === username) && (pw === password)) {
                 alert("FLAG_CMRCET_3{ W3AK_P4SSW0RDS_AR3_D4NG3R }");
